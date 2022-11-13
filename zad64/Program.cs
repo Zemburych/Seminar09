d0c6290d1cc9b75300ad66708eb8bba7998d4b3c
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-14 01:57:49 +0300

    решили задачу 64

--- zad64/Program.cs
+++ zad64/Program.cs
@@ -1,4 +1,13 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, 
+﻿// Задача 64: Задайте значение N. Напишите программу,
 // которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
 Console.Clear();
+
+Console.Write("Введите N - ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string vyvod(int n)
+{
+    return (n == 1 ? "1" : vyvod(n - 1) + n);
+}
+Console.WriteLine(vyvod(n));
